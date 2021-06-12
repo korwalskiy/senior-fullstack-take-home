83f6dc3d24fdf7a23be98c38f83c6c6de9a173f8
AbdRozaq Iwashokun <krazarky@gmail.com>
2021-06-12 11:27:20 +0100

    reverse custom mysql port, unused by docker internally

--- php-soapservice/wait.sh
+++ php-soapservice/wait.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-export $(cat /app/.env | xargs)
+cat /app/.env | xargs -L 1 | sed -e 's/^/export /' >> ~/.bashrc
 
-while ! nc -z $DB_HOST $DB_PORT; do
+source ~/.bashrc
+
+while ! nc -z $DB_HOST 3306; do
     sleep 1
     echo "Waiting for database..."
 done
 
 echo "Database is live 😀, importing schema"
-mysql --user=$DB_USERNAME --password=$DB_PASSWORD --port=$DB_PORT --host=$DB_HOST $DB_NAME --default-auth=mysql_native_password < /app/config/schema.sql
+mysql --user=$DB_USERNAME --password=$DB_PASSWORD --host=$DB_HOST $DB_NAME --default-auth=mysql_native_password < /app/config/schema.sql
 
 exec "$@"
